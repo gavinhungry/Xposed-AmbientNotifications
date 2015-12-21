89b43d31ea31e74dc2a900a0ad644712952c1a51
Gavin Lloyd <gavinhungry@gmail.com>
2015-12-21 11:59:23 -0800

    Add handleLoadPackage, handleInitPackageResources

--- src/io/gavinhungry/xposed/ambientnotifications/AmbientNotifications.java
+++ src/io/gavinhungry/xposed/ambientnotifications/AmbientNotifications.java
@@ -1,8 +1,38 @@
 package io.gavinhungry.xposed.ambientnotifications;
 
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+import de.robv.android.xposed.XC_MethodReplacement;
+
 import de.robv.android.xposed.IXposedHookInitPackageResources;
-import de.robv.android.xposed.IXposedHookZygoteInit;
+import de.robv.android.xposed.callbacks.XC_InitPackageResources;
 
-public class AmbientNotifications implements IXposedHookInitPackageResources, IXposedHookZygoteInit {
+public class AmbientNotifications implements IXposedHookLoadPackage, IXposedHookInitPackageResources {
 
+  @Override
+  public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
+    if (!lpparam.packageName.equals("com.android.systemui")) {
+      return;
+    }
+
+    XposedHelpers.findAndHookMethod("com.android.systemui.doze.DozeService", lpparam.classLoader,
+      "listenForPulseSignals", boolean.class, new XC_MethodReplacement() {
+
+      @Override
+      protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+        Boolean listen = (Boolean) param.args[0];
+        return XposedHelpers.callMethod(param.thisObject, "listenForNotifications", listen);
+      }
+    });
+  }
+
+  @Override
+  public void handleInitPackageResources(XC_InitPackageResources.InitPackageResourcesParam ipparam) throws Throwable {
+    if (!ipparam.packageName.equals("com.android.systemui")) {
+      return;
+    }
+
+    ipparam.res.setReplacement("com.android.systemui", "integer", "doze_pulse_duration_visible", 6000);
+  }
 }
